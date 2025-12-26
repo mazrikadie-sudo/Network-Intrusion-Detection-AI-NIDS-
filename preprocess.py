23a89bd5d47b63f2b8993084f2998db2e8f5c2f6
Mazri khadidja <mazrikadie@gmail.com>
2025-12-26 21:58:51 +0100

    preprocess.py

---
+++ preprocess.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+def preprocess(df):
+    X = df.drop("label", axis=1)
+    y = df["label"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.25, random_state=42
+    )
+
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+    return X_train, X_test, y_train, y_test, scaler
